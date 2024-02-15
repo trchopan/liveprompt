8eae00649f1dbfa0b84ba9ce456f5804158558ac
Chop TRAN <m@chop.dev>
2024-02-15 21:19:16 +0700

    Add favicon

--- lib/liveprompt_web.ex
+++ lib/liveprompt_web.ex
@@ -17,7 +17,7 @@ defmodule LivepromptWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon favicon.ico robots.txt)
 
   def router do
     quote do
